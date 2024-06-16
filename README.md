fcbeabbdd5d4a34625cfe389644eec41363d2a63
Linus Horn <linushorn@gmx.net>
2024-06-16 15:55:04 +0200

    complete rewrite

--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 ### How to run
 1. Clone the repo
 2. Build the docker image with `docker build -t static-server .`
-3. Run the docker image with `docker run -p [ext_port]:7995 static-server`
+3. Run the docker image with `docker run -p [ext_port]:1950 static-server`
 4. Done! You can now access the server at `localhost:[ext_port]`
--- main.py
+++ main.py
@@ -1,207 +1,57 @@
 from fastapi import FastAPI
-from fastapi.requests import Request
-from fastapi.responses import FileResponse, HTMLResponse, JSONResponse
+from fastapi.responses import FileResponse, JSONResponse
 import uvicorn
-import hashlib
-import json
-import datetime
+
 import os
+
+port = 1950
+address = "0.0.0.0"
+domain_prefix = "https://static.linus-minus-sinus.org/"
 
 app = FastAPI()
 
-#static file server
+_all_f = os.listdir(".")
+dirs = [f for f in _all_f if os.path.isdir(f) and not f.startswith(".")]
 
-@app.get("/static/{file_name}")
-async def static_file(file_name: str) -> FileResponse:
-    file_path = f"./static/{file_name}"
-    file_path = os.path.abspath(file_path)
+def is_valid_file(file_path: str) -> bool:
+    is_in_dir = False
+    for d in dirs:
+        if file_path.startswith(d):
+            is_in_dir = True
+            break
     
-    #check if file extension is allowed
-    not_allowd = ["token"]
-    if file_name.split(".")[-1] in not_allowd:
-        return JSONResponse({"message": "file extension not allowed"}, status_code=403)
+    if ".." in file_path or not is_in_dir:
+        return False
+    elif not os.path.exists(file_path):
+        return False
     
-    #check if file exists
-    if not os.path.exists(file_path):
-        return JSONResponse({"message": "file not found"}, status_code=404)
+    return True
+
+@app.get("/get_files")
+async def get_files() -> JSONResponse:
+    file_urls = []
+    for root, dirs, files in os.walk("."):
+        for f in files:
+            fp = os.path.join(root, f).replace("\\", "/")
+
+            if fp.startswith("./"):
+                fp = fp[2:]
+
+            url = f"{domain_prefix}{fp}"
+
+            if is_valid_file(fp):
+                file_urls.append(url)
     
-    #return file
+    return JSONResponse(content={"files": file_urls})
+
+
+
+@app.get("/{file_path:path}")
+async def read_file(file_path: str) -> FileResponse:
+    if not is_valid_file(file_path):
+        return JSONResponse(content={"error": "Invalid file path"}, status_code=400)
+    
     return FileResponse(file_path)
 
-
-
-# Sources UI for presentation and other source quoting
-# sources are stored in a json file
-
-class SourceEntry:
-    tag: str
-    name: str
-    link: str
-    date: str
-    
-    author: str | None
-    author_link: str | None
-    
-    description: str | None
-    
-    def __init__(self):
-        self.tag = ""
-        self.name = ""
-        self.link = ""
-        self.date = ""
-        
-        self.author = None
-        self.author_link = None
-        
-        self.description = None
-    
-    def from_json(self, data: dict):
-        self.tag = data["tag"]
-        self.name = data["name"]
-        self.link = data["link"]
-        self.date = data["date"]
-        
-        self.author = data.get("author", None)
-        self.author_link = data.get("author_link", None)
-        
-        self.description = data.get("description", None)
-        
-    def to_json(self) -> dict:
-        return {
-            "tag": self.tag,
-            "name": self.name,
-            "link": self.link,
-            "date": self.date,
-            
-            "author": self.author,
-            "author_link": self.author_link,
-            
-            "description": self.description
-        }
-    
-class Sources:
-    entries: list[SourceEntry]
-    last_update: str
-    hashed_password: str
-    project_name: str
-    
-    authors: list[str]
-    
-    def __init__(self):
-        self.entries = []
-        self.last_update = ""
-        self.hashed_password = ""
-        self.project_name = ""
-        
-        self.authors = []
-    
-    def from_json(self, data: dict):
-        self.last_update = data["last_update"]
-        self.hashed_password = data["hashed_password"]
-        self.project_name = data["project_name"]
-        
-        self.authors = data["authors"]
-        
-        for entry in data["entries"]:
-            source_entry = SourceEntry()
-            source_entry.from_json(entry)
-            self.entries.append(source_entry)
-            
-        
-    def to_json(self) -> dict:
-        entries = []
-        for entry in self.entries:
-            entries.append(entry.to_json())
-        
-        return {
-            "last_update": self.last_update,
-            "hashed_password": self.hashed_password,
-            "project_name": self.project_name,
-            
-            "authors": self.authors,
-            
-            "entries": entries
-        }
-    
-
-@app.get("/sources/json/{file_path}")
-async def sources_json(file_path: str) -> JSONResponse:
-    file_path = f"./sources/{file_path}"
-    file_path = os.path.abspath(file_path)
-    
-    #check if file exists
-    if not os.path.exists(file_path):
-        return JSONResponse({"message": "file not found"}, status_code=404)
-    
-    #load file
-    with open(file_path, "r") as file:
-        data = json.load(file)
-    
-    #create sources object
-    sources = Sources()
-    sources.from_json(data)
-    
-    #return json
-    return JSONResponse(sources.to_json())
-
-
-@app.post("/sources/{file_path}/add")
-async def add_source(file_path: str, request: Request) -> JSONResponse:
-    file_path = f"./sources/{file_path}"
-    file_path = os.path.abspath(file_path)
-    
-    #check if file exists
-    if not os.path.exists(file_path):
-        return JSONResponse({"message": "file not found"}, status_code=404)
-    
-    #load file
-    with open(file_path, "r") as file:
-        data = json.load(file)
-    
-    #create sources object
-    sources = Sources()
-    sources.from_json(data)
-    
-    #check if password is correct
-    password = request.headers.get("password", "")
-    if not hashlib.sha256(password.encode()).hexdigest() == sources.hashed_password:
-        return JSONResponse({"message": "wrong password"}, status_code=403)
-    
-    #get data from request
-    data = await request.json()
-    
-    #create new source entry
-    source_entry = SourceEntry()
-    source_entry.from_json(data)
-    
-    #add source entry to sources
-    sources.entries.append(source_entry)
-    
-    #save sources
-    with open(file_path, "w") as file:
-        json.dump(sources.to_json(), file, indent=4)
-    
-    #return json
-    return JSONResponse(sources.to_json())
-
-
-@app.get("/sources/html/{file_path}")
-async def sources_html(file_path: str) -> HTMLResponse:
-    with open("./sources.html", "r") as file:
-        html = file.read()
-    
-    json_file_path = f"/sources/json/{file_path}"
-    
-    html = html.replace("JSON_FILE_PATH_PLACEHOLDER", json_file_path)
-    
-    return HTMLResponse(html)
-
-
-
-#main page
-@app.get("/")
-async def root():
-    return {"message": "linushorn.dev static file server"}
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=7995)
+    uvicorn.run(app, host=address, port=port)
